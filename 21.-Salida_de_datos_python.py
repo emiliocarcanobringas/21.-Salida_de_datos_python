ee7d215dad54a61c43229f5aee120d197c8db60f
Emilio Carcaño Bringas <emiliocarcano@uninaciones.edu.mx>
2020-12-03 20:40:53 -0600

    Add files via upload

---
+++ 21.-Salida_de_datos_python.py
@@ -0,0 +1,14 @@
+'''
+Este programa muestra la salida de datos, con 3 formatos distintos. Para introducir variables dentro del
+texto.
+'''
+
+numero = 3
+cuadrado = 9
+print(cuadrado,"es el cuadrado de",numero,)
+
+print("{} es el cuadrado de {}".format(cuadrado,numero))
+
+print(f"{cuadrado} es el cuadrado de {numero}")
+
+# Este programa fue escrito por Emilio Carcaño Bringas
